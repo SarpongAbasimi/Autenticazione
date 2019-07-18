373938860b58514995e919a4d2fc80f6ff95199d
sarpong <abasimisarpong@rocketmail.com>
2019-07-18 20:04:49 +0100

    tested dev env for app

--- app/tests/config_test.py
+++ app/tests/config_test.py
@@ -1,2 +1,14 @@
-from app.config import Config 
+from app.config import Config, DevelopmentConfig, TestingConfig
 
+class TestConfigSetting():
+
+  def test_Cofigurations(self):
+    assert Config.DEBUG == False
+    assert Config.TESTING == False
+  
+  def test_development_configurations(self):
+    assert DevelopmentConfig.DEBUG == True
+    assert DevelopmentConfig.ENV == 'development'
+  
+  def test_testing_configurations(self):
+    assert TestingConfig.DEBUG == True
