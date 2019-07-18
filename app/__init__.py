73227e8f91eabf64fa2a4f9d4ce2a5ba90a84cb7
sarpong <abasimisarpong@rocketmail.com>
2019-07-18 21:09:40 +0100

    registred main blue print

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from app.config import config
 
+
 def create_app(config_name='development'):
   app = Flask(__name__)
   app.config.from_object(config[config_name])
 
+  from app.main.routes import main
+  app.register_blueprint(main)
   return app
