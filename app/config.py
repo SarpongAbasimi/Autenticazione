7924a88832a7ed334e9a2dff5389ab105fa163b3
sarpong <abasimisarpong@rocketmail.com>
2019-07-18 20:14:20 +0100

    using dot-env in cong file

--- app/config.py
+++ app/config.py
@@ -1,9 +1,13 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config(object):
   DEBUG = False
   TESTING = False
 
 class DevelopmentConfig(Config):
-  ENV = 'development'
+  ENV = os.getenv('DEVELOPMENT_ENV')
   DEBUG = True
 
 class TestingConfig(Config):
