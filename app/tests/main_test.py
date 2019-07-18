1c15c066b0853f19b9782a370629aa9253e5336e
sarpong <abasimisarpong@rocketmail.com>
2019-07-18 21:14:16 +0100

    wrote test for app hope page

--- app/tests/main_test.py
+++ app/tests/main_test.py
@@ -1,2 +1,8 @@
+import pytest
+
+@pytest.mark.usefixtures('client')
 class TestMainRoutes(object):
-  pass
+  
+  def test_home_page(self, client):
+    response = client.get('/')
+    assert response.status_code == 200
