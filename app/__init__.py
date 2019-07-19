6f0d3842f4f6bdfd9673477d0b16c64007d4d691
sarpong <abasimisarpong@rocketmail.com>
2019-07-19 19:43:39 +0100

    added dev db uri

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from app.config import config
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_name='development'):
   app = Flask(__name__)
@@ -12,6 +14,7 @@ def create_app(config_name='development'):
 
   from app.main.routes import main
   from app.auth.routes import auth
+
 
   app.register_blueprint(main)
   app.register_blueprint(auth, url_prefix='/auth')
--- app/tests/config_test.py
+++ app/tests/config_test.py
@@ -7,8 +7,10 @@ class TestConfigSetting():
     assert Config.TESTING == False
   
   def test_development_configurations(self):
+    db_uri = hasattr(DevelopmentConfig, 'SQLALCHEMY_DATABASE_URI')
     assert DevelopmentConfig.DEBUG == 'True'
     assert DevelopmentConfig.ENV == 'development'
+    assert db_uri == True
   
   def test_testing_configurations(self):
     assert TestingConfig.DEBUG == 'True'
