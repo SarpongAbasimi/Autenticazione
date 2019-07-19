bdb57490176a2fbcfedd21dc0a9d973def11554d
sarpong <abasimisarpong@rocketmail.com>
2019-07-19 21:33:57 +0100

    deleted flask migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,14 @@
 from flask import Flask
 from app.config import config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app(config_name='development'):
   app = Flask(__name__)
   app.config.from_object(config[config_name])
 
   db.init_app(app)
-  migrate.init_app(app, db)
 
   from app.main.routes import main
   from app.auth.routes import auth
