7bf20105aaeb322e64eb4155b37a8f2e1a200aad
sarpong <abasimisarpong@rocketmail.com>
2019-07-20 14:55:37 +0100

    created a form class

---
+++ app/auth/form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class SignUpForm(FlaskForm):
+
+  name = StringField('name', validators=[DataRequired(), Length(min=4, max=20)])
+  email = StringField('email', Email('Please enter your email address.'))
+  password = PasswordField('password', validators=[DataRequired()])
+  confrim_password = PasswordField('confirm password', EqualTo(password))
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,15 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET'])
 def login():
   return render_template('login.html')
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET'])
 def new():
   return render_template('signup.html')
+
+@auth.route('/sigup', methods=['POST'])
+def create():
+  return redirect(url_for('auth.login'))
