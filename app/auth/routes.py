ca39556ccfb5022c9818ccd016654fdbd9ce3895
sarpong <abasimisarpong@rocketmail.com>
2019-07-22 20:41:12 +0100

    added a session route for when a user login is authenticated

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,13 +1,29 @@
 from app import db
 from app.models import User
 from flask import Flask, Blueprint, render_template, redirect, url_for, request, flash
-from .form import SignUpForm
+from flask_login import login_user
+from .form import SignUpForm, LoginForm
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET'])
 def login():
-  return render_template('login.html')
+  loginform = LoginForm()
+  return render_template('login.html', loginform=loginform)
+
+@auth.route('/session', methods=['POST'])
+def new_session():
+  loginform = LoginForm()
+  if request.method == 'POST' and loginform.validate_on_submit():
+    user = User.query.filter_by(email= loginform.email.data).first()
+    if user and user.check_password(loginform.password.data):
+      login_user(user)
+      next = request.args.get('next')
+      if next is None:
+        next = url_for('main.index')
+      return redirect(url_for(next))
+    flash('Invalid email or password.')
+  return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods=['GET'])
 def new():
@@ -25,5 +41,6 @@ def create():
       db.session.commit()
       flash('You were successfully registered.')
       return redirect(url_for('auth.login'))
-  return render_template('signup.html', form=sign_up_form )
+    flash('Invalid username or password.')
+  return render_template('signup.html', form=sign_up_form)
   
