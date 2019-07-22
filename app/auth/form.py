e4e73bbfc15ee3a00072611036dde9f6840d5be3
sarpong <abasimisarpong@rocketmail.com>
2019-07-22 20:39:31 +0100

    created a login form

--- app/auth/form.py
+++ app/auth/form.py
@@ -5,7 +5,13 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class SignUpForm(FlaskForm):
 
   name = StringField('name', validators=[DataRequired(), Length(min=4, max=20)])
-  email = StringField('email', validators=[Email('Please enter your email address.')])
+  email = StringField('email', validators=[ DataRequired(), Email('Please enter your email address.') ])
   password = PasswordField('password', validators=[DataRequired(), Length(min=4, max=8)])
   confirm_password = PasswordField('confirm password', validators=[EqualTo('password')])
+  submit = SubmitField('submit')
+
+class LoginForm(FlaskForm):
+
+  email = StringField('email', validators=[ DataRequired(), Email('Please a valid email address') ])
+  password = PasswordField('password', validators=[ DataRequired()])
   submit = SubmitField('submit')
