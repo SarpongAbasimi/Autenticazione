e478c2571eb632a7fa9530d0ece931b97909a7c7
sarpong <abasimisarpong@rocketmail.com>
2019-07-22 20:42:16 +0100

    added passed user mixin to user model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from app import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(120), nullable=False)
   email = db.Column(db.String(120), nullable=False)
