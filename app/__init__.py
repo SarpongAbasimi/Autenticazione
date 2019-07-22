994d594dc5681e981386bd60e8bc9d3e2437710a
sarpong <abasimisarpong@rocketmail.com>
2019-07-22 20:39:04 +0100

    added flask login to application

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from app.config import config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config_name='development'):
   app = Flask(__name__)
   app.config.from_object(config[config_name])
 
   db.init_app(app)
+  login_manager.init_app(app)
 
   from app.main.routes import main
   from app.auth.routes import auth
