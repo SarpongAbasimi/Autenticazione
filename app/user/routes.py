188e4f747bcb5c888cb9224843d395405f3e59e8
sarpong <abasimisarpong@rocketmail.com>
2019-07-25 19:59:49 +0100

    created a  user profile page

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 user = Blueprint('user', __name__)
 
-
 @user.route('/<username>')
+@login_required
 def profile(username):
   return render_template('profile.html',username=username)
