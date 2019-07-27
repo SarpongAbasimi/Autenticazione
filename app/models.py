01afdc094aa415e96b654fcdb3b090fb3920e07a
sarpong <abasimisarpong@rocketmail.com>
2019-07-27 17:41:00 +0100

    updated model with Post

--- app/models.py
+++ app/models.py
@@ -28,3 +28,6 @@ class Post(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   content = db.Column(db.Text, nullable=False)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  def __repr__(self):
+    return f'Post => id: {self.user_id}, content: {self.content}'
