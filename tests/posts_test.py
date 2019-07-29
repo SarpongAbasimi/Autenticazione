804274e24bcaec31b06172e802ceef7863908ae8
sarpong <abasimisarpong@rocketmail.com>
2019-07-29 14:07:46 +0100

    wrote test for creating thepost blue print

--- tests/posts_test.py
+++ tests/posts_test.py
@@ -3,6 +3,6 @@ import pytest
 @pytest.mark.usefixtures('client')
 class TestPost(object):
 
-  def test_posts_page(self):
-    response = client.get('/altonero')
+  def test_posts_page(self, client):
+    response = client.get('/altonero/')
     assert b'posts page' in response.data
