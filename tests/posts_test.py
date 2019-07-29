074edefe4df5da29ba7fbef5b80acf193dddb097
sarpong <abasimisarpong@rocketmail.com>
2019-07-29 15:58:56 +0100

    wrote test to create form

--- tests/posts_test.py
+++ tests/posts_test.py
@@ -4,5 +4,12 @@ import pytest
 class TestPost(object):
 
   def test_posts_page(self, client):
-    response = client.get('/altonero/')
-    assert b'posts page' in response.data
+    response = client.get('/altonero/', follow_redirects=True)
+    assert b'Please log in to access this page.' in response.data
+
+  @pytest.mark.skip(reason='I need to first login to access this page')
+  def test_post_data(self, client):
+    response = client.post('/altonero/create',
+    data=dict(content='I love food'),
+    follow_redirects=True)
+    assert b'Please log in to access this page.' in response.data
